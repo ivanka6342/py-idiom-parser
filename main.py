c08d5ab127544af8ea90cc4d45ab46961ed4f64c
ivanka6342 <ivanka6342@mail.ru>
2022-02-21 01:24:26 +0300

    GUI update
    Replace Tkinter library with PyQt5
    Tkinter is insufficient, ugly and
    has platform-specific things

--- main.py
+++ main.py
@@ -1,110 +1,150 @@
 #!/usr/bin/env python3
 
 import parser_controller
-import platform, os
-import tkinter as tk
-from tkinter import filedialog as fd
+from PyQt5.QtWidgets import (
+    QAction,
+    QApplication,
+    QFileDialog,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QMenu,
+    QMessageBox,
+    QPushButton,
+    QStatusBar,
+    QTextBrowser,
+    QVBoxLayout,
+    QWidget
+)
+from PyQt5.QtGui import QIcon
 from requests import exceptions as req_ex
+import sys
+import os
+
+icon_path = r'icons8-book-64.png'
 
 
-# self inherits Tkinter()
-class Project_X(tk.Tk):
+class Project_X(QMainWindow):
     search_line = None
     search_btn = None
-    res_lbl = None
+    res_text = None
     filename = None
+    permanentStatusLbl = None
+    statusbar = None
 
     def __init__(self):
         super().__init__()
+        self.setWindowTitle('Project-x')
+        self.setGeometry(300, 300, 720, 480)
+        self.setWindowIcon(QIcon(icon_path))
         self.initUI()
-        self.config(menu=self.menuBar)
-
-        self.title("Project-x")
-        self['bg']='#5ec4cd'
-        if platform.system() == "Linux":
-            self.attributes('-zoomed', True)
-        elif platform.system() == "Windows":
-            self.state('zoomed')
-        else:
-            self.geometry('720x720')
 
     def initUI(self):
-        # first widget - menu. at the top
-        self.menuBar = tk.Menu(master=self)
-        self.filemenu = tk.Menu(self.menuBar, tearoff=0)
-        self.filemenu.add_command(label="Open", command=self.openfile)
-        self.filemenu.add_separator()
-        self.filemenu.add_command(label="Save", command=self.savefile)
-        self.menuBar.add_cascade(label="File", menu=self.filemenu)
+        self._createMenu()
+        self._createStatusBar()
+        self._addWidgetsToLayout()
 
-        # create search stuff
-        search_frame = tk.Frame(self)
-        search_frame.pack(side=tk.TOP, fill=tk.X, expand=False)
+    def _createMenu(self):
+        menuBar = self.menuBar()
+        
+        fileMenu = QMenu("&File", self)
+        menuBar.addMenu(fileMenu)
 
-        self.search_line = tk.Entry(search_frame, width=10)
-        self.search_line.pack(side=tk.LEFT, fill=tk.X, padx=10, pady=10, expand=True)
-        self.search_line.bind("<Return>", lambda e: self.search_btn_clicked())
-        self.search_line.focus()
+        openAction = QAction("&Open", self)
+        saveAction = QAction("&Save", self)
+        openAction.triggered.connect(self.openfile)
+        saveAction.triggered.connect(self.savefile)
+        fileMenu.addAction(openAction)
+        fileMenu.addAction(saveAction)
 
-        self.search_btn = tk.Button(search_frame, text="Search", bg="#39e639", command=self.search_btn_clicked)
-        self.search_btn.pack(side=tk.RIGHT, fill=tk.NONE, padx=10, pady=10, expand=False)
+        aboutMenu = QMenu("&About", self)
+        menuBar.addMenu(aboutMenu)
+        aboutAction = QAction("&about", self)
+        aboutMenu.addAction(aboutAction)
 
-        # create results view
-        result_frame = tk.Frame(self)
-        result_frame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+    def _createStatusBar(self):
+        self.statusbar = QStatusBar()
+        self.statusbar.showMessage("Status Bar")
+        self.setStatusBar(self.statusbar)
+        self.permanentStatusLbl = QLabel(f"{self.permanentStatus()}")
+        self.statusbar.addPermanentWidget(self.permanentStatusLbl)
 
-        self.res_lbl = tk.Text(result_frame)
-        self.res_lbl.bind("<Key>", lambda e: self.ctrlEvent(e))
-        self.res_lbl.pack(side=tk.BOTTOM, fill=tk.BOTH, padx=10, pady=10, ipadx=10, ipady=10, expand=True)
+    def _addWidgetsToLayout(self):
+        centralWidget = QWidget()
+        mainLayout = QVBoxLayout()
+        centralWidget.setLayout(mainLayout)
+        self.setCentralWidget(centralWidget)
 
+        search_box = QWidget()
+        search_box_l = QHBoxLayout()
+        search_box.setLayout(search_box_l)
+
+        self.search_line = QLineEdit()
+        search_box_l.addWidget(self.search_line)
+        self.search_line.returnPressed.connect(self.search_btn_clicked)
+
+        self.search_btn = QPushButton("Search")
+        search_box_l.addWidget(self.search_btn)
+        self.search_btn.clicked.connect(self.search_btn_clicked)
+
+        mainLayout.addWidget(search_box)
+
+        self.res_text = QTextBrowser()
+        self.res_text.setReadOnly(True)
+        mainLayout.addWidget(self.res_text)
+
+    def permanentStatus(self):
+        return "permanent status"
 
     def openfile(self):
         cwd = os.getcwd()
-        self.filename = fd.askopenfilename(title='Open a file', initialdir=cwd, filetypes=(('text files', '.txt'),('all files', '*')))
+        fname = QFileDialog.getOpenFileName(self, 'Open file', cwd, "Text files (*.txt)")[0]
         file_content = ''
-        with open(self.filename, 'r') as f:
+        with open(fname, 'r') as f:
             file_content = f.read()
-        self.res_lbl.delete('1.0', tk.END)
-        self.res_lbl.insert(tk.END, file_content)
-
+        self.res_text.setText(file_content)
+        self.statusbar.showMessage(f"open file: {fname}")
 
     def savefile(self):
         cwd = os.getcwd()
-        savefile_name = fd.asksaveasfilename(title='Save file', initialdir=cwd, filetypes=(('text files', '.txt'),('all files', '*')))
+        savefile_name = QFileDialog.getSaveFileName(self, 'Save file', cwd, "Text files (*.txt)")[0]
         with open(savefile_name, 'w+') as f:
-            f.write(self.res_lbl.get("1.0", tk.END))
-        self.filename = savefile_name
+            f.write(self.res_text.toPlainText())
+        self.statusbar.showMessage(f"save file: {savefile_name}")
 
-
-    # forbid editings for result text field, except ctrl+c to copy
-    def ctrlEvent(self, event):
-        if (12 == event.state and event.keysym == 'c' ):
-            return
-        else:
-            return "break"
-
+    def show_msgBox(self, severityLvl=QMessageBox.Warning, text="no info"):
+        msgBox = QMessageBox()
+        msgBox.setWindowTitle("Error occurred")
+        msgBox.setIcon(severityLvl)
+        msgBox.setText(text)
+        msgBox.setStandardButtons(QMessageBox.Ok)
+        msgBox.exec_()
 
     def search_btn_clicked(self):
-        self.res_lbl.delete('1.0', tk.END)
-        search_string = self.search_line.get()
+        self.res_text.clear()
+        search_string = self.search_line.text()
+
         try:
             for parser in parser_controller.project_x_parsers:
                 res = parser(search_string)
-                self.res_lbl.insert(tk.END, 'site: ' + res['domain'] + '\n\n')
-                for i,idiom in enumerate(res['idioms']):
-                    self.res_lbl.insert(tk.END, str(i) + '. ' + idiom['title'] + '\n')
-                    self.res_lbl.insert(tk.END, str(idiom['description']) + '\n')
-                    self.res_lbl.insert(tk.END, idiom['example'] + '\n')
-                    self.res_lbl.insert(tk.END, '--------------' + '\n')
+                self.res_text.append('site: ' + res['domain'] + '\n')
+                for i, idiom in enumerate(res['idioms']):
+                    self.res_text.append(str(i) + '. ' + idiom['title'] + '\n')
+                    self.res_text.append(str(idiom['description']) + '\n')
+                    self.res_text.append(idiom['example'] + '\n')
+                    self.res_text.append('--------------' + '\n')
         except req_ex as e:
-            tk.messagebox.showinfo('Error occurred', e)
+            self.show_msgBox(severityLvl=QMessageBox.Warning, text=e)
         except BaseException as b:
-            tk.messagebox.showinfo('Error occurred', b)
+            self.show_msgBox(severityLvl=QMessageBox.Warning, text=b)
 
 
 def main():
+    app = QApplication(sys.argv)
     ui = Project_X()
-    ui.mainloop()
+    ui.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
